1e3522b1957af545d8b8f4aba474c9ff06de06af
Shradha Gupta <64976961+shradha280@users.noreply.github.com>
2021-03-31 15:52:56 +0530

    Add files via upload
    step 4: it supports new lines also.

--- StrCal.java
+++ StrCal.java
@@ -4,18 +4,19 @@ import org.junit.Test;
 import shradha.tdd.StrCal;
  public class StrCal {
  @Test
-    public final void anyNumbers() {
-        Assert.assertEquals(3+9+1+4+7, StringCalculator.Add("3,9,1,4,7"));
+    public final void newLines() {
+        Assert.assertEquals(3+9+1, StrCal.Add("3,9n1"));
     }
 
    public static int add(final String num) {
     int total= 0;
-    String[] array = num.split(",");
+    String[] array = num.split(",|n");
     
     for (String num : array) {
-        if (!number.trim().isEmpty()) { 
+        if (!num.trim().isEmpty()) { 
             total+= Integer.parseInt(num);
         }
     }
     return total;
+}
 }
