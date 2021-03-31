8bf5bba902b7cbadebc7e1a0585b3e0476158151
Shradha Gupta <64976961+shradha280@users.noreply.github.com>
2021-03-31 15:47:48 +0530

    Add files via upload
    Step 3: method take any amountof numbers and return the sum

--- StrCal.java
+++ StrCal.java
@@ -2,31 +2,20 @@ package shradha.tddtest;
  
 import org.junit.Test;
 import shradha.tdd.StrCal;
- 
-public class StrCalTesting {
-    @Test
-    public final void oneNumberreturnSameValue() {
-    Assert.assertEquals(3, StrCal.add("3"));
- }
- 
-    @Test
-    public final void TwoNumbers() {
-    Assert.assertEquals(3+6, StrCal.Add("3,6"));
-}
-    @Test
-    public final void emptyStringReturn0() {
-    Assert.assertEquals(0, StrCal.Add(""));
-}
-}
-public class StrCal {
-    public static final void Add(final String num) {
-int total =0;
-        String[] array= num.split(",");
-            for (String num : array) {
-        if (!num.trim().isEmpty()) { 
+ public class StrCal {
+ @Test
+    public final void anyNumbers() {
+        Assert.assertEquals(3+9+1+4+7, StringCalculator.Add("3,9,1,4,7"));
+    }
+
+   public static int add(final String num) {
+    int total= 0;
+    String[] array = num.split(",");
+    
+    for (String num : array) {
+        if (!number.trim().isEmpty()) { 
             total+= Integer.parseInt(num);
         }
     }
     return total;
-}
 }
