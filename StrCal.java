e77ac1ba9fc2fa33db3954cf039bb8069fc9516f
Shradha Gupta <64976961+shradha280@users.noreply.github.com>
2021-03-31 16:03:09 +0530

    Add files via upload
    step 5: It supports different delimiters

--- StrCal.java
+++ StrCal.java
@@ -5,16 +5,25 @@ import shradha.tdd.StrCal;
  public class StrCal {
  @Test
     public final void newLines() {
-        Assert.assertEquals(3+9+1, StrCal.Add("3,9n1"));
+        Assert.assertEquals(3+1, StrCal.Add("//;n3;1"));
     }
-
-   public static int add(final String num) {
+   public static int Add(final String num) {
+    String delim = ",|n";
+    String noDelim= num;
+    if (num.startsWith("//")) {
+        int index = num.indexOf("//") + 2;
+        delim = num.substring(index, index + 1);
+        noDelim= num.substring(num.indexOf("n") + 1);
+    }
+    return add(noDelim, delim);
+}
+   public static int Add(final String num, final string delim) {
     int total= 0;
-    String[] array = num.split(",|n");
+    String[] array = num.split(delim);
     
     for (String num : array) {
         if (!num.trim().isEmpty()) { 
-            total+= Integer.parseInt(num);
+            total+= Integer.parseInt(num.trim());
         }
     }
     return total;
